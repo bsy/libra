4663813c3f5f68403a5b6812e74b371833bd085f
Ben Yee <ben@squaretwo.co>
2017-03-21 12:26:42 -0700

    fix test and add more implementation

--- lib/libra/inspect/worker.ex
+++ lib/libra/inspect/worker.ex
@@ -1,13 +1,48 @@
 defmodule Libra.Inspect.Worker do
   use GenServer
-  alias Libra.Page
+  alias Libra.{Asset, Page, Client}
 
   def start_link(url) do
     GenServer.start_link(__MODULE__, url)
   end
 
+  def get_page(pid) do
+    GenServer.call(pid, :get_page)
+  end
+
   def init(url) do
       page = %Page{url: url}
+      GenServer.cast(self(), :inspect_page)
       {:ok, page}
   end
+
+  # add overrides for default callback
+  def handle_call(:get_page, _from, page) do
+    {:reply, page, page}
+  end
+
+  def handle_cast(:inspect_page, page) do
+    case Client.fetch_page(page.url) do
+      {:ok, result} ->
+        page = %{page | size: result.size,
+                        assets: build_assets(result, page.url),
+                        inserted_at: Ecto.DateTime.utc(),
+                        updated_at: Ecto.DateTime.utc()}
+        {:noreply, page}
+      _error ->
+        {:stop, :error_fetching_url}
+    end
+
+  end
+
+  defp build_assets(result, url) do
+    (result.stylesheets ++ result.scripts)
+    |> Enum.map(fn(asset) ->
+      asset_url = Asset.expand_url(asset, url)
+      %Asset{id: Ecto.UUID.generate(),
+             url: asset_url,
+             status: :unfetched}
+    end)
+  end
+
 end
--- test/libra/worker_test.exs
+++ test/libra/worker_test.exs
@@ -1,17 +1,15 @@
 defmodule Libra.Inspect.WorkerTest do
   use ExUnit.Case
 
-  alias Libra.Worker
+  alias Libra.Inspect.Worker
 
   describe "fetches a url" do
     test "read a page" do
-      test "read a page" do
-        {:ok, pid} = Worker.start_link("http://localhost:4001")
+      {:ok, pid} = Worker.start_link("http://localhost:4001")
 
-        page = Worker.get_page(pid)
-        assert(page.url == "http://localhost:4001")
-        assert(page.size > 0)
-      end
+      page = Worker.get_page(pid)
+      assert(page.url == "http://localhost:4001")
+      assert(page.size > 0)
     end
   end
 end
